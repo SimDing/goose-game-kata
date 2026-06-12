3048c429a65e0422582c3c48d8c6500d81e166d3
Simon Ding <me@simonding.de>
2026-06-12 13:53:03 +0000

    Split code in main method

--- src/com/sd/goose/Main.java
+++ src/com/sd/goose/Main.java
@@ -13,8 +13,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length > 0 && args[0].equals("test")) {
             new Test().test();
-            return;
+        } else {
+            runGame();
         }
+    }
+
+    private static void runGame() throws IOException {
         GoosePresenter presenter = new GoosePresenter();
         GooseGame game = new GooseGame(presenter);
         GooseController controller = new GooseController(game, presenter);
